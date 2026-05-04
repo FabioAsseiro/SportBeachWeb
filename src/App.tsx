5d7528339986a3b134386c0604b2da8c1f472424
Fabio <fabioasseiro@hotmail.com>
2026-05-04 16:07:30 -0300

    SportBeach

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bem vindo ao Sport Beach</h1>
+      <p>Aplicativo focado para atividades esportivas na areia.</p>
+    </div>
+  )
+}
+
+export default App
